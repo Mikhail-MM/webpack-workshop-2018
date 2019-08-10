a25d8af8ec9ea8f3abe19e8881fa4d740b202eb5
Mikhail Maslyuk <leakymee@bu.edu>
2019-08-10 19:23:53 -0400

    Edit modules to demonstrate tree shaking

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import youCanNameItAnything from "./nav";
 import { top, bottom } from "./footer";
 import makeButton from "./commonjsbutton";
-import { red, blue, makeColorStyle } from "./buttonstyles";
+import {  makeColorStyle } from "./buttonstyles";
 
 console.log("Yeet");
 console.log(youCanNameItAnything);
 console.log(top);
 console.log(makeButton("My First Button!"));
 
-console.log(makeButton(21))
-console.log(red);
-console.log(makeColorStyle(red));
+console.log(makeButton("reee"))
+console.log(makeColorStyle("cyan"));
